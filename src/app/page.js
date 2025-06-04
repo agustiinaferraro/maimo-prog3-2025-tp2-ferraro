cccd6527124407576f2bfedc51d1507ef294e609
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-04 18:11:13 -0300

    saque los estilos de la page oficial

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HomeContainer from "@/components/HomeContainer";
 export default function Home() {
   return (
     
-    <div className="col-span-4 bg-sky-800 flex justify-center items-center">
+    <div>
         <HomeContainer />
     </div>
   );
