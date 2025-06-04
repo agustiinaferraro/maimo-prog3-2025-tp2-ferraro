f2ddade330d0a2a543144de5929b16ca4c7e22fc
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-03 21:25:48 -0300

    Descripción breve de los cambios

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,8 @@ import HomeContainer from "@/components/HomeContainer";
 export default function Home() {
   return (
     
-    <div>
+    <div className="col-span-4 bg-sky-800 flex justify-center items-center">
         <HomeContainer />
-      
-      <div className="grid grid-cols-12 gap-2">
-        <div className="col-span-4 bg-sky-800 flex justify-center items-center">hola</div>
-        <div className="col-span-4">chau</div>
-        <div className="col-span-4">robert</div>
-      </div>
-   
     </div>
   );
 }
