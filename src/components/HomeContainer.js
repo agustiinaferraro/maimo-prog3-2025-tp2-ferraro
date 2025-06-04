fcb2ebf11dd97956c6662766bbab0e0060909f95
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-04 17:37:30 -0300

    saque el full w de la imagen

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -35,11 +35,11 @@ const HomeContainer = () => {
   if (!page) return <div className="text-center">No se encontró la película</div>; 
 
   return (
-    <main className="flex flex-col"> {/*
+    <main className="flex flex-col items-center"> {/*
     flex flex-col: organiza los hijos en columna
     items-center: centra horizontalmente*/}
       <section className="w-full">
-        <Image src="/assets/banner.jpg" alt="banner" width={1550} height={250} className="w-full h-[250px] object-cover rounded-none"/>
+        <Image src="/assets/banner.jpg" alt="banner" width={1550} height={250} className="h-[250px] object-cover rounded-none"/>
       </section>
 
       <div className="px-4 py-8 items-center">
