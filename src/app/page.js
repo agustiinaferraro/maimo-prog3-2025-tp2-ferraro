653f96b009493b610dc440d05b83cdf86ee53c91
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-03 21:27:48 -0300

    prueba tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import HomeContainer from "@/components/HomeContainer";
 export default function Home() {
   return (
     
-    <div className="col-span-4 bg-sky-800 flex justify-center items-center">
-        <HomeContainer />
+    <div>
+        <HomeContainer className="col-span-4 bg-sky-800 flex justify-center items-center" />
     </div>
   );
 }
