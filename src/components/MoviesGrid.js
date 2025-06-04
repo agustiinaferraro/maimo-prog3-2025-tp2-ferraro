69e8e369fe910e57f74c8c8e0133e23182de77b5
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-04 18:44:12 -0300

    prueba grilla cinco

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,29 +5,27 @@ import Link from 'next/link';
 const MoviesGrid = ({ movies }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-4">
-      <div className="grilla">
-        {movies.map((movie) =>   
+      {movies.map((movie) => (
         <div key={movie.id} className="imgContainer">
-            <Link href={`/movie/${movie.id}`}>
-              <a>
-                <h2>{movie.title}</h2>
-                  <Image
-                    className="img"
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    alt={movie.original_title}
-                    width={300} 
-                    priority
-                  /> {/* imagen de la pelicula */}
-                    <div className="nombre">
-                      <ul>
-                        <li>{movie.original_title}</li>
-                      </ul>
-                    </div>
-                </a>
-            </Link>
-          </div>
-        )}    
-      </div>          
+          <Link href={`/movie/${movie.id}`}>
+            <a>
+              <h2>{movie.title}</h2>
+              <Image
+                className="img"
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.original_title}
+                width={300}
+                priority
+              />
+              <div className="nombre">
+                <ul>
+                  <li>{movie.original_title}</li>
+                </ul>
+              </div>
+            </a>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
