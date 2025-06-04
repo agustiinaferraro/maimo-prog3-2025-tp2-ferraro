435a597de4b557cad32a4362892569a2f82fefd3
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-04 02:08:47 -0300

    creacion de page en id

---
+++ src/app/movie/[id]/page.js
@@ -0,0 +1,20 @@
+import MovieContainer from "@/components/MovieContainer";
+
+//se ejecuta cuando se carga la pag de una pelicula
+export default async function MoviePage({ params }) {
+  const apiKey = "https://api.themoviedb.org/3/discover/movie?api_key=eb7e3fd7272143562cec959061b5eb32&with_genres=27";
+
+  const id = params.id;   //extrae el id de la URL
+
+  //solicitud a la api para obtener los detalles de la pelicula
+  const movieData = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=es-ES`);
+
+  const movie = await movieData.json(); //convierte la respuesta en un objeto javascript (JSON)
+
+  return ( // devuelve eel componente con los datos de la peli
+    <div className="col-span-4 bg-sky-800 flex justify-center items-center">
+      {/* Le pasa el objeto de la peli al componente Moviecontainer */}
+      <MovieContainer movie={movie} />
+    </div>
+  );
+}
