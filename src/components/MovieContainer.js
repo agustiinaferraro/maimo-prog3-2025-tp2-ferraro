6c140885f152bbeb8d89c5550b7001bf43610782
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-04 01:51:44 -0300

    movie container detalles

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Loading from "./Loading";
+import Link from 'next/link';
 
-const MovieContainer = () => {
+const MovieContainer = ({ movie }) => {
+    if (!movie) return <Loading />;
+
   return (
-    <div>MovieContainer</div>
-  )
-}
+    <div className="peliculaDetalles">
+      <h2>{movie.original_title}</h2>
+
+   
+        <ul>
+          <li>Fecha de estreno: {movie.release_date}</li>
+          <li>Idioma original: {movie.original_language}</li>
+          <li>Descripción: {movie.overview}</li>
+        </ul>
+
+      <Link href="/">
+        <button className="btn-back">Volver</button>
+      </Link>  
+    </div>
+  );
+};
 
 export default MovieContainer
--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -14,23 +14,19 @@ const MoviesGrid = ({ movies }) => {
                   <Image
                     className="img"
                     src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    alt={movie.title}
+                    alt={movie.original_title}
                     width={300} 
                     priority
                   /> {/* imagen de la pelicula */}
-                    <div className="descripcion">
+                    <div className="nombre">
                       <ul>
-                        <li>Fecha de estreno: {movie.release_date}</li>
-                        <li>Idioma original: {movie.original_language}</li>
-                        </ul>
+                        <li>{movie.original_title}</li>
+                      </ul>
                     </div>
                 </a>
             </Link>
           </div>
-        )}
-          <Link href="/" className="btn-back">
-           Volver
-          </Link>      
+        )}    
       </div>          
     </div>
   );
