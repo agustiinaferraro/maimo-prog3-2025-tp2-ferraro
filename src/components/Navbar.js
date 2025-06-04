93fb230e5d85a7853066747ab22601ecb767298c
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-04 17:41:33 -0300

    saque el margen inferior al nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     
-    <div className='flex justify-between items-center mb-4 bg-sky-600 lg:bg-lime-600'>
+    <div className='flex justify-between items-center bg-sky-600 lg:bg-lime-600'>
         <div>Logo</div>
 
         <nav> 
