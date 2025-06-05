1873b6ffff32f51c8ea148733d2a77d73215ce8a
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-05 18:21:06 -0300

    fondo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-black to-transparent">
-        <div className="text-center text-3xl text-white font-bold  py-16">
-            <h2>Movies</h2>
-            <p>© Copyright 2025</p>
+        <div className="text-center text-white py-16">
+            <h2 className="text-3xl font-bold">Movies</h2>
+            <p className="text-1xl">© Copyright 2025</p>
         </div>
     </footer>
   )
