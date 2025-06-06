dd02c955a257509a9b4903572a991ab3cb23c878
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-05 21:15:21 -0300

    hover 4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,11 +11,11 @@ const Navbar = () => {
         </div>
 
         <nav> 
-            <ul className="flex gap-6 text-white-600 hover:text-blue-500 active:bg-blue-600">
-              <li>
+            <ul className="flex gap-6 text-white-600">
+              <li className=" hover:text-blue-500 active:bg-blue-600">
                 <Link href="/">Home</Link>
               </li>                
-              <li>Generos</li>
+              <li className=" hover:text-blue-500 active:bg-blue-600">Generos</li>
             </ul>
         </nav>
     </div>
