2bc6a368f4593b8d89b9545227ef177affe6813b
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-08 02:10:35 -0300

    flechas 4

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -8,8 +8,7 @@ const MoviesGrid = ({ movies }) => {
 
   const scrollLeft = () => {
     if (scrollRef.current) { 
-      // scrollRef es una referencia para acceder al div, con el current agarro al div, sin eso agarro una caja vacía
-      // current es la propiedad de ref que permite acceder al objeto real (div en este caso)
+      // scrollRef es una referencia para acceder al div, con current agarro al div
       scrollRef.current.scrollBy({ left: -300, behavior: 'smooth' }); 
       // scrollBy mueve horizontalmente 300 px, el smooth hace una animación
     }
@@ -21,7 +20,7 @@ const MoviesGrid = ({ movies }) => {
     }
   };
 
-  const scrollRef = useRef(null); // referencia, inicialmente sin apuntar a nada o sea null
+  const scrollRef = useRef(null); // referencia, inicialmente sin apuntar a nada (null)
 
   const router = useRouter();
   const handleMovieClick = (id) => {
@@ -39,38 +38,35 @@ const MoviesGrid = ({ movies }) => {
       >
         &#8592;
       </button>
-
-      {/* Contenedor con scroll horizontal */}
+          
+      {/* Contenedor del scroll */}
       <div 
         ref={scrollRef} 
-        className="overflow-x-auto scrollbar-hide w-full px-1 mx-2"
-      >
-        {/* Este div tiene el ancho min para que el scroll funcione */}
-        <div 
-          style={{ minWidth: `${movies.length * 220}px` }} // Fuerza el ancho min para que el contenedor sea más ancho que el padre y aparezca el scroll horizontal
-          className="flex gap-4"
-        >
-          {movies.map((movie) => (
-            <div 
-              key={movie.id} 
-              className="min-w-[200px] transition-transform duration-300 hover:scale-105 active:scale-95 cursor-pointer"
-              onClick={() => handleMovieClick(movie.id)}
-            >
-              <Image
-                className="img h-[450px] object-cover"
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.original_title}
-                width={300}
-                priority
-              />
-              <div className="nombre">
-                <ul>
-                  <li className="text-1xl text-white font-bold py-2">{movie.original_title}</li>
-                </ul>
-              </div>
+        className="overflow-x-auto scrollbar-hide px-1 mx-2 w-full" 
+        style={{ whiteSpace: 'nowrap' }} 
+        // whiteSpace nowrap para que los items no se bajen y mantengan fila única
+      > 
+        {movies.map((movie) => (
+          <div 
+            key={movie.id} 
+            className="inline-block min-w-[200px] transition-transform duration-300 hover:scale-105 active:scale-95 cursor-pointer align-top"
+            // inline-block mantiene el tamaño fijo sin achicar con flexbox
+            onClick={() => handleMovieClick(movie.id)}
+          >
+            <Image
+              className="img h-[450px] object-cover"
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.original_title}
+              width={300}
+              priority
+            />
+            <div className="nombre">
+              <ul>
+                <li className="text-1xl text-white font-bold py-2">{movie.original_title}</li>
+              </ul>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
 
       {/* Flecha derecha */}
