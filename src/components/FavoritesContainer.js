6f7aaa10e057533298e5fca02bab922344ac1a61
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-14 00:10:27 -0300

    fondo img

--- src/components/FavoritesContainer.js
+++ src/components/FavoritesContainer.js
@@ -9,42 +9,46 @@ const FavoritesContainer = () => {
   return (
     <div className='p-20'>
       <h2 className='text-amber-50 text-4xl font-semibold mt-2'>Favoritos</h2>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 py-10'>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-10'>
         {favorites.map((movie) => {
           const isFavorite = favorites.some(fav => fav.id === movie.id);
 
           return (
             <div className='py-5' key={movie.id}>
               <Image
-                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                src={`https://image.tmdb.org/t/p/w500${movie.image}`}
                 alt={movie.title}
                 width={400}
                 height={200}
                 style={{ borderRadius: '4px' }}
               />
-              <h2 className='text-white'>{movie.title}</h2>
-            
-                  {isFavorite ? (
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault();
-                        deleteToFavorites(movie.id);
-                      }}
-                      className="text-2xl text-white text-right px-2 py-1"
-                    >
-                      ⭐
-                    </button>
-                  ) : (
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault();
-                        handleAddToFavorites(movie.title, movie.backdrop_path , movie.id);
-                      }}
-                      className="text-3xl text-white text-right px-2 py-1"
-                    >
-                      ☆
-                    </button>
-                  )}
+
+              <h2 className='text-white mt-2'>{movie.title}</h2>
+
+              <div className="flex justify-end">
+                {isFavorite ? (
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      deleteToFavorites(movie.id);
+                    }}
+                    className="text-2xl text-white px-2 py-1"
+                  >
+                    ⭐
+                  </button>
+                ) : (
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleAddToFavorites(movie.title, movie.backdrop_path, movie.id);
+                    }}
+                    className="text-3xl text-white px-2 py-1"
+                  >
+                    ☆
+                  </button>
+                )}
+              </div>
             </div>
           );
         })}
