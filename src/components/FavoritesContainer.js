fa44fb285e2444345fd6602b99ddc604262587c7
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-13 21:04:18 -0300

    favoritos tailwind 2

--- src/components/FavoritesContainer.js
+++ src/components/FavoritesContainer.js
@@ -7,20 +7,23 @@ const FavoritesContainer = () => {
   const { favorites } = useAppContext()
 
   return (
-    <div className='flex'>
-      {favorites.map((movie) => (
-        <div key={(movie.id)}>
-          <Image
-            src={`https://image.tmdb.org/t/p/w500${movie.image}`}
-            alt={movie.title}
-            width={400}
-            height={200}
-            style={{ borderRadius: '8px' }}
-          />
-          <h2 className='text-white'>{movie.title}</h2>
-        </div>
-      ))}
-    </div>
+  <div>
+    <h2 className='text-amber-50 w-3xl'>Favoritos</h2>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4'>
+        {favorites.map((movie) => (
+          <div key={(movie.id)}>
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.image}`}
+              alt={movie.title}
+              width={400}
+              height={200}
+              style={{ borderRadius: '8px' }}
+            />
+            <h2 className='text-white'>{movie.title}</h2>
+          </div>
+        ))}
+      </div>
+  </div>
   )
 }
 
