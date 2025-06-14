ae78d2ce4577e5ed64b6c50d258a9c41ab32a867
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-14 14:26:38 -0300

    fetch real

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -9,6 +9,9 @@ import Image from 'next/image'
 
 const HomeContainer = () => {
   const [movies, setMovie] = useState(null); //aca se guardan las respuestas de las peliculas
+  const [popular, setPopular] = useState([]);
+  const [rated, setRated] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
   const [error, setError] = useState(false); //controla si hubo algun error en la ca;rga
   const [loading, setLoading] = useState(true);
 
@@ -16,10 +19,22 @@ const HomeContainer = () => {
     const fetchMovie = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(
-          'https://api.themoviedb.org/3/discover/movie?api_key=eb7e3fd7272143562cec959061b5eb32'
-        );
-        setMovie(response.data); //guarda en data los datos de las peliculas
+        const apiKey = 'eb7e3fd7272143562cec959061b5eb32';
+        
+        //desestructuración de arrays, saca los valores de un array y los guarda en variables
+        const [response, resPopular, resRated, resUpcoming] = await  Promise.all //promise.all agrupa varias promesas al mismo tiempo
+        ([ 
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`), //todas las movies
+        axios.get(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`),
+        axios.get(`https://api.themoviedb.org/3/tv/top_rated?api_key=${apiKey}`),
+        axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`),
+        ]);
+        //el.results es porque tengo muchas peticiones
+        setMovie(response.data.results); //guarda en data los datos de las peliculas
+        setPopular(resPopular.data.results); //populares
+        setRated(resRated.data.results); //mejor valoradas
+        setUpcoming(resUpcoming.data.results); //proximas
+
         setLoading(false); //se pone en falso, deja de cargar
       } catch (error) {
         console.log('Error en la API:', error);
@@ -34,31 +49,26 @@ const HomeContainer = () => {
   if (loading) return <Loading />; //si carga muestra el icono del componente loading
   if (error) return <div className="mx-auto h-screen text-3xl text-white text-center">Hubo un error</div>;
   if (!movies) return <div className="mx-auto h-screen text-3xl text-white text-center">No se encontró la película</div>; 
-  const principales = movies.results.slice(0,7);
-  const destacadas = movies.results.slice(8,14);
-  const recomendadas = movies.results.slice(15,23);
 
   return (
-    <main className="flex flex-col items-center"> {/*
-    flex flex-col: organiza los hijos en columna
-    items-center: centra horizontalmente*/}
-     
-    {!loading && <Hero movies={movies.results} />} 
-    {loading && <Loading />}
+    <main className="flex flex-col items-center">
 
-      <div className="px-6 py-8 items-center md:w-5/5 overflow-x-hidden">
-        <div>
-          <h2 className="text-left text-3xl text-white font-bold  py-6">Principales</h2>
-          <MoviesGrid movies={principales} />
+    <Hero movies={movies} />
 
-          <h2 className="text-left text-3xl text-white font-bold  py-6">Destacadas</h2>
-          <MoviesGrid movies={destacadas} useBackdrop={false} />
+    <div className="px-6 py-8 items-center md:w-5/5 overflow-x-hidden">
+      <div>
+        <h2 className="text-left text-3xl text-white font-bold py-6">Populares</h2>
+        <MoviesGrid movies={popular} />
 
-          <h2 className="text-left text-3xl text-white font-bold  py-6">Recomendadas</h2>
-          <MoviesGrid movies={recomendadas} />
-        </div>
+        <h2 className="text-left text-3xl text-white font-bold py-6">Mejor valoradas</h2>
+        <MoviesGrid movies={rated} useBackdrop={false} />
+
+        <h2 className="text-left text-3xl text-white font-bold py-6">Próximas</h2>
+        <MoviesGrid movies={upcoming} />
       </div>
-    </main>
+    </div>
+
+  </main>
   );
 };
 
