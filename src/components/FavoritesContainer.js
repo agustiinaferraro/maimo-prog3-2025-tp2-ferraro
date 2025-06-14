3f58bfd892d3d123f66ef38b9b171b2f43f32bcb
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-14 00:25:31 -0300

    fondo img 5

--- src/components/FavoritesContainer.js
+++ src/components/FavoritesContainer.js
@@ -4,49 +4,50 @@ import { useAppContext } from '@/app/context/AppContext'
 import Image from 'next/image'
 
 const FavoritesContainer = () => {
-  const { favorites, deleteToFavorites } = useAppContext();
+  const { favorites, handleAddToFavorites, deleteToFavorites } = useAppContext();
 
   return (
     <div className='p-20'>
       <h2 className='text-amber-50 text-4xl font-semibold mt-2'>Favoritos</h2>
-
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-10'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 py-10'>
         {favorites.map((movie) => {
-          // Validar que backdrop_path exista
-          const imageUrl = movie.backdrop_path
-            ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
-            : '/fallback.jpg'; // O cualquier imagen de respaldo
+          const isFavorite = favorites.some(fav => fav.id === movie.id);
 
           return (
-            <div className='py-5' key={movie.id} style={{ width: 400 }}>
-              {/* contenedor relativo y con tamaño fijo */}
-              <div className="relative w-[400px] h-[225px] rounded-md overflow-hidden">
-                <Image
-                  src={imageUrl}
-                  alt={movie.title}
-                  fill // para que la imagen llene el contenedor
-                  style={{ objectFit: 'cover' }}
-                  sizes="(max-width: 768px) 100vw, 400px"
-                  priority={true}
-                  onError={(e) => {
-                    e.currentTarget.src = '/fallback.jpg'; // fallback si falla
-                  }}
-                />
-              </div>
-
-              <h2 className='text-white mt-2'>{movie.title}</h2>
-
+            <div className='py-5' key={movie.id}>
+              <Image
+                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                alt={movie.title}
+                width={400}
+                height={200}
+                style={{ borderRadius: '4px' }}
+              />
+              <h2 className='text-white'>{movie.title}</h2>
+            
               <div className="flex justify-end">
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    deleteToFavorites(movie.id);
-                  }}
-                  className="text-2xl text-white text-right px-2 py-1"
-                >
-                  ⭐
-                </button>
-              </div>
+                  {isFavorite ? (
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        deleteToFavorites(movie.id);
+                      }}
+                      className="text-2xl text-white text-right px-2 py-1"
+                    >
+                      ⭐
+                    </button>
+                  ) : (
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleAddToFavorites(movie.title, movie.backdrop_path , movie.id);
+                      }}
+                      className="text-3xl text-white text-right px-2 py-1"
+                    >
+                      ☆
+                    </button>
+                  )}
+                </div>
+
             </div>
           );
         })}
