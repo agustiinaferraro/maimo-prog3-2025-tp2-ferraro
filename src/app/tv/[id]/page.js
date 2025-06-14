a25e698db8164153dc951bf9e38faa6b2a510790
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-14 16:09:12 -0300

    page tv

---
+++ src/app/tv/[id]/page.js
@@ -0,0 +1,17 @@
+import MovieContainer from "@/components/MovieContainer";
+
+//se ejecuta cuando se carga la pag de una pelicula
+export default async function TvPage({ params }) {
+  const apiKey = "eb7e3fd7272143562cec959061b5eb32";
+
+ const id = params.id;
+
+  const tvData = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=es-ES`);
+  const tvShow = await tvData.json();
+
+  return (
+    <div className="col-span-4 w-full bg-black bg-opacity-70 flex justify-center items-center">
+      <MovieContainer movie={tvShow} />
+    </div>
+  );
+}
