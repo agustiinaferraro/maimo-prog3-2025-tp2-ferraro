85fdc913e3b988b22412b72a9da7719fbab515dc
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-13 22:15:54 -0300

    fav borrar 2

--- src/app/context/AppContext.js
+++ src/app/context/AppContext.js
@@ -1,51 +1,49 @@
 'use client'
 
-import {useState, useEffect, useContext, createContext} from 'react';
+import { useState, useEffect, useContext, createContext } from 'react';
 const AppContext = createContext()
 
-export const AppContextProvider = ({children}) => {
-  const [favorites, setFavorites] = useState ([]);
-  
+export const AppContextProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
   //aca va mi logica y funciones custom
-  
-  useEffect (()=>{
+
+  useEffect(() => {
     console.log(favorites) //el console.log es asincronico. si quiero ver algun valor tengo que ponerlo en el useefecr y ponerlo en el array
   }, [favorites]) //este es el array
-  
-  //aca ver si es titile u origin title algo asi
-   const handleAddToFavorites = (title, image, id) => {
-    setFavorites([...favorites, {title, image, id}])
-   };
 
-   const deleteToFavorites = (borrar) => {
+  //aca ver si es titile u origin title algo asi
+  const handleAddToFavorites = (title, image, id) => {
+    setFavorites([...favorites, { title, image, id }])
+  };
+
+  const deleteToFavorites = (borrar) => {
     const newFavorites = favorites.filter(movie => movie.id !== borrar);  //el filter crea un nuevo array solo con las peliculas que cumplan con la condicion (todas las peliculas cuyo id sean distintos a "borrar")
-    setFavorites (newFavorites) //actualizo setfavorites
-   };
+    setFavorites(newFavorites) //actualizo setfavorites
+  };
 
   const favoritesQty = () => favorites.length //ver cuantos favoritos hay
+
   return (
     <AppContext.Provider
-    value={{
+      value={{
         favorites,
         handleAddToFavorites,
+        deleteToFavorites, 
         favoritesQty
-    }}
+      }}
     >
-    {children}
+      {children}
     </AppContext.Provider>
-    
   )
-
 }
 
-
-
-
-export const  useAppContext = () => {
-    const context = useContext(AppContext)
-    if(!context) {
-        throw new Error ('error')
-    }
-    return context;
+export const useAppContext = () => {
+  const context = useContext(AppContext)
+  if (!context) {
+    throw new Error('error')
+  }
+  return context;
 }
+
 export default AppContext
